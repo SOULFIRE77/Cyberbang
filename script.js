67c03e4bd4791514a25652cde7a58bb8959887ce
SOULFIRE77 <curmeid4@gmail.com>
2025-06-14 22:52:30 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+import { TonConnectUI } from "@tonconnect/ui";
+
+const tonConnectUI = new TonConnectUI({
+  manifestUrl: 'https://soulfire77.github.io/cyberbang/tonconnect-manifest.json',
+  buttonRootId: 'ton-connect'
+});
