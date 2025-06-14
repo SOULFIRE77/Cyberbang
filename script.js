2776700d49c0439c101d18c835b89471804f9718
SOULFIRE77 <curmeid4@gmail.com>
2025-06-15 00:09:21 +0300

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-import { TonConnectUI } from "@tonconnect/ui";
-
-const tonConnectUI = new TonConnectUI({
-  manifestUrl: 'https://soulfire77.github.io/cyberbang/tonconnect-manifest.json',
-  buttonRootId: 'ton-connect'
-});
