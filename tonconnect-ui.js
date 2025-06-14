65d7b3d20096cb3b4398034f551bc5ac8dcd44f1
SOULFIRE77 <curmeid4@gmail.com>
2025-06-14 22:19:49 +0300

    Delete tonconnect-ui.js

--- tonconnect-ui.js
+++
@@ -1,7 +0,0 @@
-// Файл для подключения TonConnect UI через CDN
-(function(){
-  const script = document.createElement('script');
-  script.src = "https://unpkg.com/@tonconnect/ui@latest/dist/tonconnect-ui.min.js";
-  script.onload = () => console.log("TonConnect UI loaded");
-  document.head.appendChild(script);
-})();
