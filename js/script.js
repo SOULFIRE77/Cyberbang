12509bbcfef49496099bcd4cbfec658fe6a46826
SOULFIRE77 <curmeid4@gmail.com>
2025-06-15 21:17:31 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,18 +3,16 @@ document.addEventListener('DOMContentLoaded', () => {
     console.error("TonConnect UI не загружен");
     return;
   }
-  const manifestUrl = "https://soulfire77.github.io/cyberbang/tonconnect-manifest.json";
   const tonConnectUI = new TON_CONNECT_UI.TonConnectUI({
-  manifestUrl: "https://soulfire77.github.io/cyberbang/tonconnect-manifest.json",
-  buttonRootId: "ton-connect"
-});
-
+    manifestUrl: "https://soulfire77.github.io/cyberbang/tonconnect-manifest.json",
+    buttonRootId: "ton-connect"
+  });
   async function onWalletConnected(provider) {
     try {
       const account = (await provider.getAccount())?.account;
       if (account) {
         document.getElementById("wallet-address").innerText = "Подключен адрес: " + account;
-        document.getElementById("register-instructions").innerHTML = `Скопируйте команду:<br><code>/register ${account}</code>`;
+        document.getElementById("register-instructions").innerHTML = `<code>/register ${account}</code>`;
         const botLink = document.getElementById("bot-link");
         botLink.href = "https://t.me/CyberBangBOT";
         botLink.style.display = "inline-block";
@@ -23,7 +21,6 @@ document.addEventListener('DOMContentLoaded', () => {
       console.error("Ошибка при получении адреса:", err);
     }
   }
-
   tonConnectUI.onStatusChange(async (wallet) => {
     if (wallet.connected) {
       const provider = await tonConnectUI.connect();
