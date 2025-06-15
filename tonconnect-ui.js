036881eec1aa54d7c68ddb0e16904cb43d5cd93e
SOULFIRE77 <curmeid4@gmail.com>
2025-06-15 11:13:06 +0300

    Delete tonconnect-ui.js

--- tonconnect-ui.js
+++
@@ -1,7 +0,0 @@
-// Loader for TonConnect UI
-(function(){
-  const script = document.createElement('script');
-  script.src = "https://unpkg.com/@tonconnect/ui@latest/dist/tonconnect-ui.min.js";
-  script.onload = () => console.log("TonConnect UI loaded");
-  document.head.appendChild(script);
-})();
